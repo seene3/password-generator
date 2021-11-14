044695a27e2b78fa8a97ad98de36fc27dab4b786
Sean Gillespie <seene3@gmail.com>
2021-11-13 19:12:42 -0500

    added getRandom functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,23 @@
 // Assignment code here
+function getRandomLower() {
+  var lower = 'abcdefghijklmnopqrstuvwxyz';
+  return lower[Math.floor(Math.random() * lower.length)];
+}
 
+function getRandomUpper() {
+  var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  return upper[Math.floor(Math.random() * upper.length)];
+}
+
+function getRandomNumber() {
+  var number = '0123456789';
+  return number[Math.floor(Math.random() * number.length)];
+}
+
+function getRandomSymbol() {
+  var symbol = '!@#$%^&*?(){}[]=<>/';
+  return symbol[Math.floor(Math.random() * symbol.length)];
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
