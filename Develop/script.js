e6892bf2072064c521c71147dea9f7a4e1c11fad
Sean Gillespie <seene3@gmail.com>
2021-11-13 20:59:00 -0500

    added password functions

--- Develop/script.js
+++ Develop/script.js
@@ -19,6 +19,50 @@ function getRandomSymbol() {
   return symbol[Math.floor(Math.random() * symbol.length)];
 }
 
+function generatePassword() {
+  const length = getPasswordLength();
+  let password = '';
+  for (var i = 0; i < length; i++) {
+    var getType = Math.floor(Math.random() * 4)
+    switch (getType) {
+      case 1:
+        password += getRandomLower();
+        break;
+      case 2:
+        password += getRandomUpper();
+        break;
+      case 3:
+        password += getRandomNumber();
+        break;
+      case 0:
+        password += getRandomSymbol();
+        break;
+      default:
+        console.log("error");
+        break;
+    }
+  }
+  if (password.length === length) {
+  return password;
+  }
+}
+
+var getPasswordLength = function() {
+  var passwordLength = "";
+  var parseLength;
+
+  while (passwordLength == "" || passwordLength == null) {
+    passwordLength = prompt("How long would you like your password to be between 8 and 128 characters?");
+  }
+  parseLength = parseInt(passwordLength);
+  if (parseLength > 7 && parseLength < 129) {
+    return parseLength;
+  }
+  else {
+    window.alert("Please put in valid range");
+    getPasswordLength();
+  }
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
